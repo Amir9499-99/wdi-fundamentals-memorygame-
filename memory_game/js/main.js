f59d0e8aa4191f8d1412ef4b3d4f78da32e201c4
Amir Ishanov <amirishanov@Amirs-MBP.attlocal.net>
2020-01-15 14:39:05 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
+{
+console.log("User flipped" + " " + cardOne);
+console.log("User flipped" + " " + cardTwo);
+}
+
+if (cardsInPlay.length === 2){
+  if (cardsInPlay[0] === cardsInPlay[0])
+     alert ("You found a match!");
+   else
+     alert ("Sorry, try again!");
+}
