105bb6c442cf9973ea661c5d31d45bc9b60cece0
Amir Ishanov <amirishanov@Amirs-MBP.attlocal.net>
2020-01-17 21:17:56 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,47 @@
-const cards = ["queen", "queen", "king", "king"];
+
+const cards = [
+ {
+   rank : "queen",
+   suit : "hearts",
+   cardImage : "images/queen-of-hearts.png"
+ },
+ {
+   rank : "queen",
+   suit : "diamonds",
+   cardImage : "images/queen-of-diamonds.png"
+ },
+ {
+   rank : "king",
+   suit : "hearts",
+   cardImage : "images/king-of-hearts.png"
+ },
+ {
+   rank : "king",
+   suit : "diamonds",
+   cardImage : "images/king-of-diamonds.png"
+ }
+ ];
+
 let cardsInPlay = [];
 
 function checkForMatch(){
    if (cardsInPlay[0] === cardsInPlay[1]) {
-     console.log("You found a match!");
+     alert("You found a match!");
  } else {
-     console.log("Sorry, try again!");
+     alert("Sorry, try again!");
  }
 };
 
 
 function flipCard(cardsId) {
-  cardsInPlay.push(cards[cardsId]);
-  console.log("User flipped " + cards[cardsId]);
-  if (cardsInPlay.length > 1) {
+  cardsInPlay.push(cards[cardsId].rank);
+  cardsInPlay.push(cards[cardsId].cardImage);
+  cardsInPlay.push(cards[cardsId].suit);
+  console.log("User flipped " + cards[cardsId].rank + " of " + cards[cardsId].cardImage + " "+ cards[cardsId].suit);
+    if (cardsInPlay.length > 3) {
     checkForMatch();
-  }
+    }
 };
 
 flipCard(0);
+flipCard(1);
